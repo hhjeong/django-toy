2013ffdebab0e8c79a99f2d67e61f2133147eeda
JeongHyunhwan <hyunhwan@172.20.nate.com>
2014-12-21 21:44:01 +0900

    a brief standing generation was completed

--- standing/views.py
+++ standing/views.py
@@ -4,15 +4,23 @@ from standing.models import Status
 from django import forms
 from django.http import HttpResponseRedirect
 from collections import defaultdict 
+from datetime import datetime, timedelta
 # Create your views here.
 
 def index(request):
     all_submissions = Status.objects.all()
 
-    standing = defaultdict( defaultdict )
-    
-    list_standing = [ ['team_10' , { 'A' : 10 ,  'B' : 40 }] ]
+    standing = defaultdict( int ) 
 
+    for a in all_submissions:
+        team_id = a.team_id
+        prob_id = a.pid
+        result = a.result
+
+        if result == "YES":
+            standing[team_id] += 1
+        
+    list_standing = standing.items()
     print list_standing
     return render(request, 'standing.html', {'list_standing':list_standing})
 
