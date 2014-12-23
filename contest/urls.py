e19850d5530babca5eb8a8c6fc4072afadd5ae7c
JeongHyunhwan <hyunhwan@JeongHyunhwans-MacBook-Pro.local>
2014-12-23 20:39:16 +0900

    change url

--- contest/urls.py
+++ contest/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^standing/', include('standing.urls')),
     url(r'^status/', include('status.urls')),
 )
